b6b4ce32de840cd84c54e30cc59ac679b7dfbe6c
Jee <jeecoding09@gmail.com>
2026-05-17 01:01:48 +0800

    Fix: fix duration initialization
    feat: attendee management methods

--- Concert.py
+++ Concert.py
@@ -3,6 +3,28 @@ from datetime import timedelta
 class Concert:
    def __init__(self, **dur):
       # Converts int into time (minutes), eto basihan natin sa ibang functions
-      self.duration = timedelta(hours=dur['hour'], minutes=dur['mins'], seconds=dur['secs'])
+      self.duration = timedelta(hours=dur['hours'], minutes=dur['mins'], seconds=dur['secs'])
+      # Stack
+      self.attendees = []
    
+   def pushAttendee():
+      pass
+   
+   def popAttendee():
+      pass
+   
+   def peekLastAttendee():
+      pass
+   
+   def getAttendanceDuration():
+      pass
+   
+   def detectRuleViolations():
+      pass
+   
+   def displayAttendees():
+      pass
+   
+   def generateAttendanceRep():
+      pass
    
