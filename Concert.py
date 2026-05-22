24a968316adda0b90b5a8551759e0cdb7d8e8d6c
Jee <jeecoding09@gmail.com>
2026-05-22 11:29:26 +0800

    feat: dummy data

--- Concert.py
+++ Concert.py
@@ -1,39 +1,47 @@
-from datetime import timedelta 
+import datetime as dt
+
 
 class Concert:
-   def __init__(self, **dur):
-      # Converts int into time (minutes), eto basihan natin sa ibang functions
-      self.duration = timedelta(hours=dur['hours'], minutes=dur['mins'], seconds=dur['secs'])
-      # Stack
-      self.attendees = []
-   
-   def pushAttendee(self):
-      pass
-   
-   def popAttendee(self):
+    def __init__(self, **dur):
+        # Converts int into time (minutes), eto basihan natin sa ibang functions
+        self.duration = dt.timedelta(
+            hours=dur["hours"], minutes=dur["mins"], seconds=dur["secs"]
+        )
+        # Stack
+        self.attendees = [
+            {"ticket": 1, "name": "Tester 1", "entryTime": None, "exitTime": None},
+            {"ticket": 1, "name": "Tester 1", "entryTime": None, "exitTime": None},
+        ]
 
-      if len(self.attendees) == 0:
-         print("[⤐ EXIT DENIED ❌ ⬷] No attendees inside the venue.")
-         return
+    def pushAttendee(self):
+        pass
 
-      attendee = self.attendees.pop()
-      attendee.exit_time = datetime.now()
+    def popAttendee(self):
 
-      print(f"[⤐ EXIT SUCCESS ✅ ⬷] '{attendee.name}' exited the venue. "
-         f"Ticket: {attendee.ticket_id} | "
-         f"Exit Time: {attendee.exit_time.strftime('%H:%M:%S')}")
-   
-   def peekLastAttendee(self):
-      pass
-   
-   def getAttendanceDuration(self):
-      pass
-   
-   def detectRuleViolations(self):
-      pass
-   
-   def displayAttendees(self):
-      pass
-   
-   def generateAttendanceRep(self):
-      pass
+        if len(self.attendees) == 0:
+            print("[⤐ EXIT DENIED ❌ ⬷] No attendees inside the venue.")
+            return
+
+        attendee = self.attendees.pop()
+        attendee.exit_time = dt.now()
+
+        print(
+            f"[⤐ EXIT SUCCESS ✅ ⬷] '{attendee.name}' exited the venue. "
+            f"Ticket: {attendee.ticket_id} | "
+            f"Exit Time: {attendee.exit_time.strftime('%H:%M:%S')}"
+        )
+
+    def peekLastAttendee(self):
+        pass
+
+    def getAttendanceDuration(self):
+        pass
+
+    def detectRuleViolations(self):
+        pass
+
+    def displayAttendees(self):
+        pass
+
+    def generateAttendanceRep(self):
+        pass
